1b94041ec56a85245f4c06fcf1d4b6379e647fbc
ExeWhyZ <araj52444@gmail.com>
2022-11-25 20:28:54 +0530

    updated heroku link

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
-    const host = "http://localhost:5000";
+    const host = "https://inote.herokuapp.com";
     const [credentials, setCredentials] = useState({ email: "", password: "", });
     let navigate = useNavigate();
 
